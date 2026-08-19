307ea7e2eb3acf82716cb81bd4c786966ec926c5
Sameer <sameer-dev-stack@users.noreply.github.com>
2026-08-19 13:00:20 +0600

    fix(middleware): skip robots.txt and sitemap.xml

--- middleware.ts
+++ middleware.ts
@@ -195,6 +195,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|api/stripe/webhook|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|api/stripe/webhook|robots\\.txt|sitemap\\.xml|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
